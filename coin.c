4ce7742b49cd4d3b9033ce1d026e69810489a46d
Musthafa Faishal <musthafafaishal@gmail.com>
2021-11-13 17:26:13 +0700

    Updates

--- coin.c
+++ coin.c
@@ -21,11 +21,11 @@ int x[koin] = { 50000, 20000, 10000, 5000, 2000, 1000, 500, 200, 100 }; // x : k
 void CoinExchange(int C, int A) 
 {
   /* kamus lokal */
-	int S[mak] = { 0 }; // S : himpunan_koin
-	int i=0, k = 0, temp=0; // temp = Total(nilai semua koin di dalam S)
+  int S[mak] = { 0 }; // S : himpunan_koin
+  int i=0, k = 0, temp=0; // temp = Total(nilai semua koin di dalam S)
 
   /* algoritma */
-	while ((temp != A) && (C != 0)) {
+  while ((temp != A) && (C != 0)) {
     C = C - x[i];
 
     if (temp + x[i] <= A) {
@@ -36,29 +36,29 @@ void CoinExchange(int C, int A)
       i++;
     }
   }
-  
+
   if (S[koin] == koin) {
     printf("%d", S);
   } else {
     printf("");
   }
 
-	for (i = 0; i < k; i++) { 
-		printf("%d ", S[i]); 
-	} 
+  for (i = 0; i < k; i++) { 
+    printf("%d ", S[i]); 
+  } 
 
-	return; 
+  return; 
 } 
 
 /* driver */
 int main(void)
 {
   /* kamus */
-	int uang;
+  int uang;
 
   /* algoritma */
-	scanf("%d", &uang); // membaca jumlah uang yang akan ditukar
-	CoinExchange(uang, uang); //panggil function
+  scanf("%d", &uang); // membaca jumlah uang yang akan ditukar
+  CoinExchange(uang, uang); //panggil function
 
-	return 0;
+  return 0;
 }
